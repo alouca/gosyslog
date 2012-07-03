614573a06d6f3c443df48f2d251ed3e5123014d0
Andreas Louca <andreas@louca.org>
2012-07-03 16:58:18 +0300

    Initial code commit

--- src/syslog.go
+++ src/syslog.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"fmt"
+	//	"fmt"
 	"net"
 	"strings"
+	"logger"
+	"time"
 	"os"
 	"strconv"
+	"database/sql"
+	_ "github.com/ziutek/mymysql/godrv"
 )
 
 const (
-	KERNEL = iota
+	KERNEL	= iota
 	USERLEVEL
 	MAIL
 	SYSTEM
@@ -35,7 +39,7 @@ const (
 )
 
 const (
-	EMERGENCY = iota
+	EMERGENCY	= iota
 	ALERT
 	CRITICAL
 	ERROR
@@ -45,11 +49,30 @@ const (
 	DEBUG
 )
 
+var (
+	l	*logger.Logger
+	db	*sql.DB
+)
+
 func main() {
+	// Init the logger
+	l = logger.CreateLogger(true, true)
+
+	go startServer(":8888")
+
 	pc, err := net.ListenPacket("udp4", ":514")
+	defer pc.Close()
 
 	if err != nil {
-		fmt.Printf("Error on listen: %s\n", err.String())
+		l.Fatal("Error on listen: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	// Connect to mysql database
+	db, err = sql.Open("mymysql", "gosyslog/root/")
+
+	if err != nil {
+		l.Fatal("Error on mysql connect: %s\n", err.Error())
 		os.Exit(1)
 	}
 
@@ -58,22 +81,26 @@ func main() {
 		readBytes, addr, err := pc.ReadFrom(buffer[0:])
 
 		if err != nil {
-			fmt.Printf("Error on read: %s\n", err.String())
+			l.Fatal("Error on read: %s\n", err.Error())
 			os.Exit(1)
 		}
 
-		fmt.Printf("Read total %d bytes from %s\n", readBytes, addr.String())
+		l.Debug("Read total %d bytes from %s\n", readBytes, addr.String())
 		message := string(buffer[0:readBytes])
 		// Retrieve Priority
-		priority := message[strings.Index(message, "<")+1:strings.Index(message, ">")]
+		priority := message[strings.Index(message, "<")+1 : strings.Index(message, ">")]
 		pri, err := strconv.Atoi(priority)
-		
+
 		facility := pri / 8
 		severity := pri % 8
-		
-		fmt.Printf("Priority: %s - Facility: %d Severity: %d\n", priority, facility, severity)
-		fmt.Printf("%s\n", message)
+
+		l.Debug("Priority: %s - Facility: %d Severity: %d\n", priority, facility, severity)
+		l.Debug("%s\n", message)
+
+		ipAddress := addr.String()
+		message = strings.TrimSpace(message[strings.Index(message, ":")+2:])
+
+		db.Exec("insert into syslog (source, timestamp, facility, severity, message) VALUES (?, ?, ?, ?, ?)", ipAddress[0:strings.Index(ipAddress, ":")], time.Now().Unix(), facility, severity, message)
 
 	}
-
 }
